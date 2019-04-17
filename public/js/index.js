3468914acfa22dfb7ebb7bb2f93ebc614fa941e2
Nathan Abriol <nathan_siegfrid@yahoo.com>
2019-04-17 12:46:17 +0800

    ui changes

--- public/js/index.js
+++ public/js/index.js
@@ -3,16 +3,38 @@ var socket = io();
 socket.on('connect', function(){
     console.log('Connected to server');
 
-    socket.emit('newChat', {
-        from: 'Nathan',
-        text: 'Hi, i need to get my steam account back'
-    });
+    // socket.emit('newChat', {
+    //     from: 'Nathan',
+    //     text: 'Hi, i need to get my steam account back'
+    // });
 });
 
 socket.on('newChat', function(chat){
     console.log('new chat', chat)
+    var li = jQuery('<li></li>');
+    li.text(`${chat.from}: ${chat.text}`);
+
+    jQuery('#messages').append(li);
 });
 
 socket.on('disconnect', function(){
     console.log('Disconnected from server');
+});
+
+// socket.emit('createChat', {
+//     from: 'Nathan',
+//     text: 'I\'ve\ retrieved back my steam account this morning'
+// }, function(data){
+//     console.log('Got it', data);
+// });
+
+jQuery("#message-form").on('submit', function(event){
+    event.preventDefault(); 
+
+    socket.emit('createChat',{
+        from: 'Nathan',
+        text: jQuery('[name=message]').val()
+    }, function(){
+
+    });
 });
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
 
-
+const { generateMessage } = require('./utils/message');
 const publicPath = path.join(__dirname, '/public');
 const port = process.env.PORT || 5000;
 const app = express();
@@ -22,8 +22,19 @@ io.on('connection', (socket)=>{
     //     createAt: 'feb 2014'
     // });
 
-    socket.on('newChat', (newChat)=>{
-        console.log('created chat', newChat);
+    socket.emit('newChat', generateMessage('Admin', 'Welcome to the chat app'));
+
+    socket.broadcast.emit('newChat', generateMessage('Admin', 'New user is joined'));
+
+    socket.on('createChat', (chat, callback)=>{
+        console.log('created chat', chat);
+        io.emit('newChat', generateMessage(chat.from, chat.text));
+        callback('This is from the server')
+        // socket.broadcast.emit('newChat', {
+        //     from: chat.from,
+        //     text: chat.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', ()=> {
